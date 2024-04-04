8eb9637ea221af42358ef7c580515f36414064dd
icceey <iceclock1037@gmail.com>
2024-04-04 19:23:20 +0800

    feat: water commit
    Co-Authored-By: Hugefiver <i@iruri.moe>

--- src/services/stream/pull_stream.rs
+++ src/services/stream/pull_stream.rs
@@ -1,26 +1,31 @@
-use rsmpeg::avformat::AVIOContext;
-use tokio::select;
+use rtmp::{
+    relay::{pull_client::PullClient, push_client::PushClient},
+    rtmp::RtmpServer,
+};
+use streamhub::StreamsHub;
 use tracing::{debug, error, info};
 
 pub async fn rtmp_server(
     conf: &crate::config::AppConfig,
-) -> anyhow::Result<Box<dyn std::error::Error>> {
+) -> anyhow::Result<(), Box<dyn std::error::Error>> {
     let listen_port = conf.stream.port;
     let listen_host = conf.stream.host.clone();
 
-    let mut listener = tokio::net::TcpListener::bind((listen_host, listen_port)).await?;
-    loop {
-        select! {
-            tcp_stream = listener.accept() => {
-                if let Ok((stream, addr)) = tcp_stream {
-                    debug!("new connection from {:?}", addr);
+    let mut stream_hub = StreamsHub::new(None);
+    let sender = stream_hub.get_hub_event_sender();
+    tokio::spawn(async move { stream_hub.run().await });
 
-                } else {
-
-                    error!("failed to accept connection");
-                }
-
-            }
+    let mut rtmp_server = RtmpServer::new(
+        listen_host + ":" + &listen_port.to_string(),
+        sender,
+        1,
+        None,
+    );
+    tokio::spawn(async move {
+        if let Err(err) = rtmp_server.run().await {
+            error!("rtmp server run error: {}", err);
         }
-    }
+    });
+
+    loop {}
 }
